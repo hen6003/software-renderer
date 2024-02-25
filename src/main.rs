fdb1d5ef072a8c323d32d037b343ba55489d48ce
Henry Smith <henrysmith6003@protonmail.com>
2024-02-25 12:55:53 +0000

    Zbuffer

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,9 @@ fn main() -> Result<(), EventLoopError> {
 
     event_loop.set_control_flow(ControlFlow::Wait);
 
+    let mut frame = 0.0;
+    let mut light_dir = Vec3::new(0.0, 0.0, -1.0);
+
     event_loop.run(move |event, elwt| {
         match event {
             Event::WindowEvent {
@@ -41,7 +44,12 @@ fn main() -> Result<(), EventLoopError> {
                 // You only need to call this if you've determined that you need to redraw in
                 // applications which do not always need to. Applications that redraw continuously
                 // can render here instead.
-                //window.request_redraw();
+
+                //light_dir = Vec3::new(0.0, 1.0 * (frame as f32).sin(), 1.0 * (frame as f32).cos());
+
+                //frame += 0.01;
+
+                window.request_redraw();
             }
             Event::WindowEvent {
                 event: WindowEvent::RedrawRequested,
@@ -72,13 +80,6 @@ fn main() -> Result<(), EventLoopError> {
                 drawer.line((20.0, 13.0), (40.0, 80.0), Srgb::new(255, 0, 0));
                 drawer.line((80.0, 40.0), (13.0, 20.0), Srgb::new(255, 0, 0));
 
-                drawer.triangle(
-                    [(10.0, 10.0), (100.0, 30.0), (190.0, 160.0)],
-                    Srgb::new(0, 0, 255),
-                );
-
-                let light_dir = Vec3::new(0.0, 0.0, -1.0);
-
                 for mesh in &scene.meshes {
                     for face in &mesh.faces {
                         let world_coords: [Vec3; 3] = [
@@ -89,9 +90,12 @@ fn main() -> Result<(), EventLoopError> {
 
                         let half_screen = drawer.screen_size.as_vec2() * Vec2::splat(0.5);
                         let screen_coords = [
-                            (world_coords[0].xy() + Vec2::ONE) * half_screen,
-                            (world_coords[1].xy() + Vec2::ONE) * half_screen,
-                            (world_coords[2].xy() + Vec2::ONE) * half_screen,
+                            ((world_coords[0].xy() + Vec2::ONE) * half_screen)
+                                .extend(world_coords[0].z),
+                            ((world_coords[1].xy() + Vec2::ONE) * half_screen)
+                                .extend(world_coords[1].z),
+                            ((world_coords[2].xy() + Vec2::ONE) * half_screen)
+                                .extend(world_coords[2].z),
                         ];
 
                         let n = (world_coords[2] - world_coords[0])
@@ -117,6 +121,7 @@ fn main() -> Result<(), EventLoopError> {
 
 struct Drawer<'a> {
     buffer: Buffer<'a, Rc<Window>, Rc<Window>>,
+    zbuffer: Vec<f32>,
     screen_size: UVec2,
 }
 
@@ -124,6 +129,7 @@ impl<'a> Drawer<'a> {
     pub fn new(buffer: Buffer<'a, Rc<Window>, Rc<Window>>, width: u32, height: u32) -> Self {
         Self {
             buffer,
+            zbuffer: vec![f32::NEG_INFINITY; (width * height) as usize],
             screen_size: (width, height).into(),
         }
     }
@@ -193,25 +199,46 @@ impl<'a> Drawer<'a> {
         }
     }
 
-    fn barycentric(pts: [UVec2; 3], p: UVec2) -> Vec3 {
-        let p = p.as_vec2();
+    //fn barycentric(pts: [UVec2; 3], p: UVec2) -> Vec3 {
+    //    let p = p.as_vec2();
 
-        let p1 = pts[0].as_vec2();
-        let p2 = pts[1].as_vec2();
-        let p3 = pts[2].as_vec2();
+    //    let p1 = pts[0].as_vec2();
+    //    let p2 = pts[1].as_vec2();
+    //    let p3 = pts[2].as_vec2();
 
-        let lambda1 = ((p2.y - p3.y) * (p.x - p3.x) + (p3.x - p2.x) * (p.y - p3.y))
-            / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
-        let lambda2 = ((p3.y - p1.y) * (p.x - p3.x) + (p1.x - p3.x) * (p.y - p3.y))
-            / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
-        let lambda3 = 1.0 - lambda1 - lambda2;
+    //    let lambda1 = ((p2.y - p3.y) * (p.x - p3.x) + (p3.x - p2.x) * (p.y - p3.y))
+    //        / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+    //    let lambda2 = ((p3.y - p1.y) * (p.x - p3.x) + (p1.x - p3.x) * (p.y - p3.y))
+    //        / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+    //    let lambda3 = 1.0 - lambda1 - lambda2;
 
-        Vec3::new(lambda1, lambda2, lambda3)
+    //    Vec3::new(lambda1, lambda2, lambda3)
+    //}
+
+    fn barycentric<P>(pts: [P; 3], p: Vec3) -> Vec3
+    where
+        P: Into<Vec3> + Copy,
+    {
+        let mut s = [Vec3::ZERO; 2];
+
+        for i in (0..2).rev() {
+            s[i][0] = pts[2].into()[i] - pts[0].into()[i];
+            s[i][1] = pts[1].into()[i] - pts[0].into()[i];
+            s[i][2] = pts[0].into()[i] - p[i];
+        }
+
+        let u = s[0].cross(s[1]);
+
+        if u.z.abs() > 1e-2 {
+            Vec3::new(1.0 - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z)
+        } else {
+            -Vec3::ONE
+        }
     }
 
     pub fn triangle<P, S, C>(&mut self, pts: [P; 3], color: C)
     where
-        P: Into<Vec2> + Copy,
+        P: Into<Vec3> + Copy,
         C: Into<palette::rgb::Rgb<S, u8>> + Copy,
         S: std::fmt::Debug,
     {
@@ -220,27 +247,30 @@ impl<'a> Drawer<'a> {
         let clamp = self.screen_size().as_vec2();
 
         for point in pts {
-            bboxmin = Vec2::ZERO.max(bboxmin.min(point.into()));
-            bboxmax = clamp.min(bboxmax.max(point.into()));
+            bboxmin = Vec2::ZERO.max(bboxmin.min(point.into().truncate()));
+            bboxmax = clamp.min(bboxmax.max(point.into().truncate()));
         }
 
         // Into integer coords
         let bboxmin = bboxmin.as_uvec2();
         let bboxmax = bboxmax.as_uvec2();
 
-        let pts = [
-            pts[0].into().as_uvec2(),
-            pts[1].into().as_uvec2(),
-            pts[2].into().as_uvec2(),
-        ];
-
         for x in bboxmin.x..bboxmax.x {
             for y in bboxmin.y..bboxmax.y {
-                let p = UVec2::new(x, y);
+                let mut p = UVec2::new(x, y).as_vec2().extend(0.0);
                 let bc_screen = Self::barycentric(pts, p);
 
                 if bc_screen.x >= 0.0 && bc_screen.y >= 0.0 && bc_screen.z >= 0.0 {
-                    self.pixel(p, color)
+                    for i in 0..3 {
+                        p.z += pts[i].into().z * bc_screen[i];
+                    }
+
+                    let width = self.screen_size().x as f32;
+                    if self.zbuffer[(p.x + p.y * width) as usize] < p.z {
+                        self.zbuffer[(p.x + p.y * width) as usize] = p.z;
+
+                        self.pixel(p.truncate().as_uvec2(), color)
+                    }
                 }
             }
         }
